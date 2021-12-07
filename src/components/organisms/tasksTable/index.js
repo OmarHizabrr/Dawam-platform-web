ad6a277173aa993517a6f1edd2bce9d76c24bcac
osama-abdullah <50793839+osama-abdullah@users.noreply.github.com>
2021-12-07 09:15:45 +0300

    final-beta

--- src/components/organisms/tasksTable/index.js
+++ src/components/organisms/tasksTable/index.js
@@ -140,6 +140,7 @@ export default function tasksTable() {
           sortOrder: sortedInfo.columnKey === 'description' && sortedInfo.order,
           ellipsis: true,
         },
+        
         {
           title: 'مدة المهمة/الإجازة',
           dataIndex: 'period',
